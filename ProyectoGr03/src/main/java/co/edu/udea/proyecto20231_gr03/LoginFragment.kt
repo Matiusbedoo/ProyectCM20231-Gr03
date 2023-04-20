7c4bc3701a6d061f548ed7e21565d573a6c00c59
Juan Sebastian Ramirez <juan.solarte@ceiba.com.co>
2023-04-19 21:18:34 -0500

    Ajustes iniciales

---
+++ ProyectoGr03/src/main/java/co/edu/udea/proyecto20231_gr03/LoginFragment.kt
@@ -0,0 +1,37 @@
+package co.edu.udea.proyecto20231_gr03
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+class LoginFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val enterButton = view.findViewById<Button>(R.id.enterButton)
+
+        enterButton.setOnClickListener{
+            findNavController().navigate(R.id.action_loginFragment_to_tipoUsuarioFragment)
+        }
+
+    }
+}
--- ProyectoGr03/src/main/java/co/edu/udea/proyecto20231_gr03/MainActivity.kt
+++ ProyectoGr03/src/main/java/co/edu/udea/proyecto20231_gr03/MainActivity.kt
@@ -2,10 +2,14 @@ package co.edu.udea.proyecto20231_gr03
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
 }
