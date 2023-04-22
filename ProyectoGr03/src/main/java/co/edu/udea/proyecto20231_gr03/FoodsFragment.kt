bdf98300400750a9c0f1baaecadb22fe24b14879
Juan Sebastian Ramírez <jramirra@contratista.epm.co>
2023-04-21 23:47:01 -0500

    merge foods manage

--- ProyectoGr03/src/main/java/co/edu/udea/proyecto20231_gr03/FoodsFragment.kt
+++ ProyectoGr03/src/main/java/co/edu/udea/proyecto20231_gr03/FoodsFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import androidx.navigation.fragment.findNavController
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +16,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [newFood.newInstance] factory method to
+ * Use the [FoodsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class newFood : Fragment() {
+class FoodsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +37,7 @@ class newFood : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_food, container, false)
+        return inflater.inflate(R.layout.fragment_foods, container, false)
     }
 
     companion object {
@@ -44,16 +47,32 @@ class newFood : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment newFood.
+         * @return A new instance of fragment foods.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            newFood().apply {
+            FoodsFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val imgBtn= view.findViewById<ImageView>(R.id.imageFood1)
+
+        imgBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_foods_to_manage)
+        }
+
+        val btnRegister= view.findViewById<Button>(R.id.btnAddFood)
+
+        btnRegister.setOnClickListener {
+            findNavController().navigate(R.id.action_foods_to_newFood)
+        }
     }
 }
